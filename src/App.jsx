df7b04ec5ca135f9a41de875f860658bd40c17fc
MSS17 <miguelsandoval20470266@gmail.com>
2023-05-31 15:45:49 -0600

    Contador

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [contador, setContador] = useState(0);
+  // const incrementarContador = () =>{
+  //   setContador(contador + 1)
+  // }
+  // const decrementarContador = () =>{
+  //   setContador(contador + 1)
+  // }
+  // const restablecerContador = () =>{
+  //   setContador(0)
+  // }
+  return (
+    <>
+      
+      <h1>Ejercicio 1 Contador</h1>
+      <div className="container">
+        <div className="card">
+          <div className="card-Header">
+            <p className='counter'>{contador}</p>
+          </div>
+          <div className="card-body">
+            <button className='subtract-btn' onClick={() => setContador(contador-1)}>-</button>
+            <button className='reset-btn' onClick={() => setContador(0)}>Restablecer</button>
+            <button className='add-btn' onClick={() => setContador(contador+1)}>+</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
